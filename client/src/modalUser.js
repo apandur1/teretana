a228836e4fd5ad4bf15088aec04fbfe153fa1891
apandur1 <apandur1@etf.unsa.ba>
2021-10-06 16:27:52 +0200

    Obnova clanarine implementirana

--- client/src/modalUser.js
+++ client/src/modalUser.js
@@ -10,6 +10,7 @@ function ModalUser(props) {
     datum_placanja: new Date(),
     datum_isteka: new Date(),
   });
+  const [obnova, setObnova] = useState("")
 
   useEffect(() => {
     fetch("http://127.0.0.1:3000/clanarina/" + props.clan.ClanarinaId)
@@ -30,7 +31,41 @@ function ModalUser(props) {
     );
   }
 
-  function obnoviClanarinu() {}
+  function obnoviClanarinu() {
+    let danas = new Date()
+    let zaMjesec = new Date()
+    zaMjesec.setDate(zaMjesec.getDate() + 30)
+    const obnoviClanarinuAjax = new XMLHttpRequest();
+    obnoviClanarinuAjax.onreadystatechange = function () {
+      if (
+        obnoviClanarinuAjax.readyState === 4 &&
+        obnoviClanarinuAjax.status === 200
+      ) {
+        setObnova("Obnovili ste članarinu!")
+        setClanarina({
+          id: props.clan.ClanarinaId,
+          datum_placanja: danas,
+          datum_isteka: zaMjesec
+        })
+      } else if (
+        obnoviClanarinuAjax.readyState === 4 &&
+        obnoviClanarinuAjax.status === 404
+      )
+        console.log("Greska");
+    };
+    obnoviClanarinuAjax.open("PUT", "http://127.0.0.1:3000/clanarina/"+props.clan.ClanarinaId);
+    obnoviClanarinuAjax.setRequestHeader(
+      "Content-Type",
+      "application/json;charset=UTF-8"
+    );
+    obnoviClanarinuAjax.send(
+      JSON.stringify({
+        id: props.clan.ClanarinaId,
+        datum_placanja: danas,
+        datum_isteka: zaMjesec
+      })
+    );
+  }
 
   return (
     <div>
@@ -46,6 +81,7 @@ function ModalUser(props) {
         <div className="bold">{pretvoriDatum(clanarina.datum_placanja)}</div>
         Datum isteka:{" "}
         <div className="bold">{pretvoriDatum(clanarina.datum_isteka)}</div>
+        {obnova}
         <footer>
           <button onClick={obnoviClanarinu}>Obnovi članarinu</button>
           <button onClick={props.onCancelButton}>Cancel</button>
